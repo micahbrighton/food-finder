cf7e4e45ae5022c29daa21790706a64a774f9c38
rohanwadhavkar <rohanwadhavkar@gmail.com>
2019-12-04 14:48:17 -0500

    cleaned up code

--- SignupCustomizations/script.js
+++ SignupCustomizations/script.js
@@ -1,10 +1,8 @@
-import getjwt, { getCookie } from "../login/script.js";
 $(function () {
     const dietsList = ["gluten", "vegetarian", "vegan", "lacto-vegetarian", "ovo-vegetarian", "ketogenic", "pescetarian", "paleo", "primal", "whole30"];
     const intolerancesList = ["dairy", "egg", "gluten2", "grain", "peanut", "seafood", "sesame", "shellfish", "soy", "sulfite", "tree-nut", "wheat"];
 
     var userIngreds = [];
-    //import getjwt() from "../login/script.js";
 
     function checked(id) {
         return document.getElementById(id).checked;
@@ -22,22 +20,7 @@ $(function () {
         intolerancesList.forEach(function (element) {
             if (checked(element)) { userIntolerances.push(element) }
         });
-        console.log(userDiets)
-        console.log(userIntolerances)
-        //  console.log(getCookie("jwt1"))
 
-
-        // let status = axios.get('http://localhost:3000/account/status',
-        //     {
-        //         headers: {
-        //             Authorization: "Bearer " + getCookie("jwt1")
-
-        //         }
-        //     });
-        // status.then(response => {
-        // console.log(getCookie("setupUserName"));
-        console.log(sessionStorage.getItem('name'));
-        console.log(sessionStorage.getItem('jwt'));
         axios.post('http://localhost:3000/user/data/',
             {
 
@@ -50,16 +33,9 @@ $(function () {
             },
             {
                 headers: { Authorization: "Bearer " + sessionStorage.getItem('jwt') }
-                // "type": "merge"
+
             });
-
-
     });
-
-
-    // userDiets = [];
-    // userIntolerances = [];
-    // userIngreds = [];
 
 });
 
