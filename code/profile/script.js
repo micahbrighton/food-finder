f1150c7e216cd7736cde49bdc10d270a711bddf8
lucaspm98 <46602692+lucaspm98@users.noreply.github.com>
2019-12-10 17:49:42 -0500

    lgout

--- code/profile/script.js
+++ code/profile/script.js
@@ -4,3 +4,8 @@ $(function () {
     document.getElementById("username").append(sessionStorage.getItem('name'));
 
 });
+
+document.querySelector('a#logout').addEventListener('click', function(event) {
+    event.preventDefault();
+    sessionStorage.removeItem('jwt');
+});
